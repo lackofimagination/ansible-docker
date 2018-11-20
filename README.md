9d02294936463da7598dcbaef8737b7376e24d79
Huang Xuxing <grubbybloodorc@gmail.com>
2018-11-20 14:34:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ansible Docker
+
+[![Build Status](https://travis-ci.org/lackofimagination/ansible-docker.svg?branch=master)](https://travis-ci.org/lackofimagination/ansible-docker)
---
+++ molecule/upload/tests/test_default.py
@@ -0,0 +1,44 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_docker_user(host):
+    u = host.user('docker')
+
+    assert u.name == 'docker'
+    assert 'docker' in u.groups
+
+
+def test_containerd_systemd_file(host):
+    f = host.file('/etc/systemd/system/containerd.service.d/override.conf')
+
+    assert f.exists
+
+
+def test_docker_systemd_options_file(host):
+    f = host.file('/etc/systemd/system/docker.service.d/options.conf')
+
+    assert f.exists
+
+
+def test_docker_systemd_environment_file(host):
+    f = host.file('/etc/systemd/system/docker.service.d/environment.conf')
+
+    assert f.exists
+
+
+def test_docker_daemon_file(host):
+    f = host.file('/etc/docker/daemon.json')
+
+    assert f.exists
+
+
+def test_docker_service(host):
+    s = host.service('docker')
+
+    assert s.is_enabled
+    assert s.is_running
